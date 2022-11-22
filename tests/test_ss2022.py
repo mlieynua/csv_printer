888eca32ca77cfd6bebd86f5410e1dd4c07c5789
mlieynua <mlieynua-trp@outlook.jp>
2022-11-22 15:53:29 +0900

    Add tests

---
+++ tests/test_ss2022.py
@@ -0,0 +1,21 @@
+import unittest
+
+from special_lecture.ss2022 import CSVPrinter
+
+
+class TestCSVPrinter(unittest.TestCase):
+
+    def test_read1(self):
+        printer = CSVPrinter("tests/sample.csv")
+        l = printer.read()
+        self.assertEqual(3, len(l))
+
+    def test_read2(self):
+        printer = CSVPrinter("tests/sample.csv")
+        l = printer.read()
+        self.assertEqual("value2B", l[1][1])
+
+    def test_read3(self):
+        with self.assertRaises(FileNotFoundError):
+            printer = CSVPrinter("no_exist.csv")
+            l = printer.read()
