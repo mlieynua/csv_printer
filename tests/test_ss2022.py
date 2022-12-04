edb10183d993f7b20c7add3a2412a645f666491c
mlieynua <mlieynua-trp@outlook.jp>
2022-12-04 22:11:30 +0900

    Fix test

--- tests/test_ss2022.py
+++ tests/test_ss2022.py
@@ -5,24 +5,9 @@ from special_lecture.ss2022 import CSVPrinter
 
 class TestCSVPrinter(unittest.TestCase):
 
-    def test_read1(self):
+    def test_read(self):
         printer = CSVPrinter("tests/sample.csv")
-        l = printer.read()
-        self.assertEqual(3, len(l))
-
-    def test_read2(self):
-        printer = CSVPrinter("tests/sample.csv")
-        l = printer.read()
-        self.assertEqual("value2B", l[1][1])
-
-    def test_read3(self):
-        with self.assertRaises(FileNotFoundError):
-            printer = CSVPrinter("no_exist.csv")
-            l = printer.read()
-
-    def test_read4(self):
-        printer = CSVPrinter("tests/sample.csv")
-        lines = printer.read()
-        self.assertEqual(3, len(lines))
-        for line in lines:
-            self.assertEqual(3, len(line))
+        results = printer.read()
+        self.assertEqual(9, len(results))
+        for result in results:
+            self.assertEqual(int, type(result))
